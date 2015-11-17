87090558f3bea900f1136e6b9d49240fd63f6913
chenli <ali322@gmail.com>
2015-11-17 19:03:35 +0800

    change index container

--- shared/chunk/index/app.jsx
+++ shared/chunk/index/app.jsx
@@ -1,18 +1,23 @@
 'use strict'
 import React from "react";
+import {bindActionCreators} from "redux";
 import {Provider,connect} from "react-redux";
 import rootReducer from "./reducer.es6";
 import createStoreWithMiddleware from "../../lib/store-creator.es6";
 import Weather from "./component.jsx";
 
-function selector(state){
-    const {weatherByCityName} = state
-    return {
-        weatherByCityName
-    };
+class WeatherWrapper extends Component{
+    render(){
+        return (
+            <Weather {...this.props} 
+            {...bindActionCreators(actions,this.props.dispatch)}/>
+        )
+    }
 }
 
-let WeatherConnected = connect(selector)(Weather);
+let WeatherConnected = connect((state)=>{
+    return state;
+})(WeatherWrapper);
 
 class WeatherApp extends React.Component{
     render(){
