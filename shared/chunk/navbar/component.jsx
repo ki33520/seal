5b4b22b811636f4155ae572f8d918754d48d8372
ouyangli <278500368@qq.com>
2016-01-04 11:52:56 +0800

    增加Tabs

--- shared/chunk/navbar/component.jsx
+++ shared/chunk/navbar/component.jsx
@@ -4,125 +4,144 @@ import React,{Component} from "react";
 import Header from "../common/header.jsx";
 import Icon from "../../component/icon.jsx";
 import classNames from "classnames";
-
-class Menu extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            activeIndex: 0
-        }
-    }
-
-    handleClick(index){
-        this.setState({
-            activeIndex:index
-        });
-    }
-
-    render(){
-        const {nav} = this.props;
-        var item = nav.map((name,i)=>{
-            const classes = classNames({
-                "current":i === this.state.activeIndex
-            });
-            return (
-                <li className={classes} key={"tab-nav-"+i} 
-                onClick={this.handleClick.bind(this,i)}><i>{name}</i></li>
-            );
-        });
-
-        return (
-            <ul>{item}</ul>
-        )
-    }
-}
-
-class Side extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            activeIndex: 0
-        }
-    }
-
-    handleClick(index){
-        this.setState({
-            activeIndex:index
-        });
-    }
-
-    render(){
-        const {nav} = this.props;
-        var item = nav.map((name,i)=>{
-            const classes = classNames("name",{
-                "current":i === this.state.activeIndex
-            });
-            return (
-                <li className={classes} key={"tab-nav-"+i} onClick={this.handleClick.bind(this,i)}>
-                    {name}
-                </li>
-            );
-        });
-
-        return (
-            <ul className="leftNav">{item}</ul>
-        )
-    }
-}
+import {Tabs,TabsItem} from "../../component/tabs.jsx";
+import {SlideTabs,SlideTabsItem} from "../../component/slidetabs.jsx";
 
 class Navbar extends Component{
-    
+ 
     handleSearch(){
         location.href="/search"
     }
     
     
     render(){
+        //var {categorys,brands,origins} = this.props;
+        var categorys=[1];
+        var brands = [1];
+        var origins = [1];
+        categorys = categorys.map((category,i)=>{
+            const key = "category-" + i;
+            return (
+                <CategoryRow category={category} key={key}/>
+            )
+        });
+        brands = brands.map((brand,i)=>{
+            const key = "brand-" + i;
+            return (
+                <BrandRow brand={brand} key={key}/>
+            )
+        });
+        origins = origins.map((origin,i)=>{
+            const key = "origin-" + i;
+            return (
+                <OriginRow origin={origin} key={key}/>
+            )
+        });
         return (
             <div className="box">
                 <Header>
-                    <div className="logo"><img src="client/asset/images/indexlogo.png" /></div>
+                    <div className="logo"><img src="/client/asset/images/indexlogo.png" /></div>
                     <div className="btn-right" onClick={this.handleSearch.bind(this)}>
                         <Icon icon="search"/>
                     </div>
                 </Header>
-                <div className="polyTabs">
-                    <Menu nav={["类别","品牌","产地"]} />
-                </div>
-                <div className="polyCon">
-                    <div id="page-content">
-                        <div className="poly_1 page-0 page-current">
-                            <div className="category-bd"></div>
-                            <Side nav={["母婴用品","美容彩妆","营养保健","家居洗护","进口美食"]} />
-                            
-                            <div className="rightCon">
-                                <div className="sh fadeIn">
-                                    <a href="#" className="ad">
-                                        <img src="/client/asset/images/pic24.gif"/>
-                                    </a>
-                                    <div className="content">
-                                        <div className="title">热门分类</div>
-                                        <a href="/" className="cg">
-                                            <img src="/client/asset/images/965_G_1445533723842.gif"/>
-                                            <div>母婴直邮</div>
-                                        </a>
-                                        <a href="/" className="cg">
-                                            <img src="/client/asset/images/965_G_1445533723842.gif"/>
-                                            <div>美妆直邮</div>
-                                        </a>
-                                        <a href="/" className="cg">
-                                            <img src="/client/asset/images/965_G_1445533723842.gif"/>
-                                            <div>保健直邮</div>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="coupon-list">
+                    <Tabs effect="slide">
+                        <TabsItem title={<i>类别</i>}>{categorys}</TabsItem>
+                        <TabsItem title={<i>品牌</i>}>{brands}</TabsItem>
+                        <TabsItem title={<i>产地</i>}>{origins}</TabsItem>
+                    </Tabs>
                 </div>
             </div>
         )
     }
 }
 
+class CategoryRow extends Component{
+
+    render(){
+        const {Category} = this.props;
+         
+        return (
+            <div>
+                <div className="category-bd"></div>
+                <ul className="leftNav">
+                    <li className="name active">母婴用品</li>
+                    <li className="name">美容彩妆</li>
+                    <li className="name">营养保健</li>
+                    <li className="name">家居洗护</li>
+                    <li className="name">进口美食</li>
+                </ul>
+                <div className="rightCon">
+                    <div className="sh fadeIn">
+                        <a href="#" className="ad">
+                            <img src="/client/asset/images/pic24.gif"/>
+                        </a>
+                        <div className="content">
+                            <div className="title">热门分类</div>
+                            <a href="/" className="cg">
+                                <img src="/client/asset/images/965_G_1445533723842.gif"/>
+                                <div>母婴直邮</div>
+                            </a>
+                            <a href="/" className="cg">
+                                <img src="/client/asset/images/965_G_1445533723842.gif"/>
+                                <div>美妆直邮</div>
+                            </a>
+                            <a href="/" className="cg">
+                                <img src="/client/asset/images/965_G_1445533723842.gif"/>
+                                <div>保健直邮</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class BrandRow extends Component{
+    render(){
+        const {brand} = this.props;
+         
+        return (
+            <div>
+                <a href="#" className="allBrand">全部品牌<i className="iconfont icon-right"></i></a>
+                <div className="title">推荐品牌</div>
+                <div className="brandList clearfix">
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                </div>
+                <div className="title">母婴用品</div>
+                <div className="brandList clearfix">
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                </div>
+                <div className="title">母婴用品</div>
+                <div className="brandList clearfix">
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                    <a href="#"><div><img src="/client/asset/images/pic25.gif"/></div></a>
+                </div>
+            </div>
+        );
+    }
+}
+
+class OriginRow extends Component{
+    render(){
+        const {origin} = this.props;
+         
+        return (
+            <div>
+                <div className="kindArea">
+                    <a href="#"><img src="/client/asset/images/area_usa.gif"/></a>
+                    <a href="#"><img src="/client/asset/images/area_korea.gif"/></a>
+                    <a href="#"><img src="/client/asset/images/area_japan.gif"/></a>
+                </div>
+            </div>
+        );
+    }
+}
 export default Navbar;
