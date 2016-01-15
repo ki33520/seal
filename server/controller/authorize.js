2bfcd73e0575fa30804a3219644a6ae07847a237
chenli <ali322@gmail.com>
2016-01-15 15:57:10 +0800

    fix authorize

--- server/controller/authorize.js
+++ server/controller/authorize.js
@@ -21,7 +21,7 @@ var loginGateway = function(req, res, next) {
                 user.memberId = resp.object.id
                 req.session.user = user;
                 if(config.runtime === "develop"){
-                    // returnUrl = returnUrl.replace(":3000",":5000")
+                    returnUrl = returnUrl.replace(":3000",":5000")
                 }
                 res.redirect(returnUrl);
             }
