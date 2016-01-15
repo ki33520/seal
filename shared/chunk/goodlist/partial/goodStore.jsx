a6b9788dc218b7d5aea80e1b5cc570548b164c2b
ouyangli <278500368@qq.com>
2016-01-15 18:10:20 +0800

    重置按钮

--- shared/chunk/goodlist/partial/goodStore.jsx
+++ shared/chunk/goodlist/partial/goodStore.jsx
@@ -9,75 +9,52 @@ import{
  
 
 class GoodSorter extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            sortType:SORT_NORMAL,
-            sortViewType:SORT_ASC
+    changeParam(type){
+        const {handleSort,params} = this.props;
+        switch(type){
+            case 'normal':
+                params.sortType = SORT_NORMAL;
+                break;
+            case 'sales':
+                params.sortType = SORT_SALES;
+                break;
+            case 'price':
+                if(params.sortViewType===SORT_DESC){
+                    params.sortViewType=SORT_ASC;
+                }else{
+                    params.sortViewType=SORT_DESC;
+                }
+                params.sortType = SORT_PRICE;
+                break;
+            default:
+                break;
         }
+        handleSort(params);
     }
-
-    orderByDefault(){
-        if(this.state.sortType===SORT_NORMAL){
-            return false;
-        }
-        this.setState({
-            sortType:SORT_NORMAL,
-            sortViewType:SORT_ASC
-        });
-        this.props.toggleSort({
-            sortType:SORT_NORMAL,
-            sortViewType:SORT_DESC
-        });
-    }
-
-    orderBySales(){
-        if(this.state.sortType===SORT_SALES){
-            return false;
-        }
-        this.setState({
-            sortType:SORT_SALES,
-            sortViewType:SORT_ASC
-        });
-        this.props.toggleSort({
-            sortType:SORT_SALES,
-            sortViewType:SORT_DESC
-        });
-    }
-
-    orderByPrice(){
-        this.setState({
-            sortType:SORT_PRICE,
-            sortViewType:this.state.sortViewType === SORT_ASC ? SORT_DESC : SORT_ASC
-        });
-        this.props.toggleSort({
-            sortType:SORT_PRICE,
-            sortViewType:this.state.sortViewType
-        });
-    }
- 
 
     render(){
-        
+        const {sortType,sortViewType} = this.props.params;
+ 
         const normalClass=classNames('normal',{
-            "active":this.state.sortType===SORT_NORMAL
+            "active":sortType===SORT_NORMAL
         });
  
         const priceClass=classNames("price",{
-            "active":this.state.sortType===SORT_PRICE && this.state.sortViewType === SORT_DESC,
-            "price-asc":this.state.sortType===SORT_PRICE && this.state.sortViewType === SORT_ASC
+
+            "active":sortType===SORT_PRICE && sortViewType === SORT_DESC,
+            "price-asc":sortType===SORT_PRICE && sortViewType === SORT_ASC
         });
 
         const salesClass=classNames('sales',{
-            "active":this.state.sortType===SORT_SALES
+            "active":sortType===SORT_SALES
         });
 
         return (
             <div>
                 <div className="search-order">
-                    <a href="javascript:;" className={normalClass} onClick={this.orderByDefault.bind(this)}><i></i>默认</a>
-                    <a href="javascript:;" className={priceClass}  onClick={this.orderByPrice.bind(this)}><i></i>价格</a>
-                    <a href="javascript:;" className={salesClass}  onClick={this.orderBySales.bind(this)}><i></i>销量</a>
+                    <a href="javascript:;" className={normalClass} onClick={this.changeParam.bind(this,'normal')}><i></i>默认</a>
+                    <a href="javascript:;" className={priceClass}  onClick={this.changeParam.bind(this,'price')}><i></i>价格</a>
+                    <a href="javascript:;" className={salesClass}  onClick={this.changeParam.bind(this,'sales')}><i></i>销量</a>
                 </div>
             </div>
         )
