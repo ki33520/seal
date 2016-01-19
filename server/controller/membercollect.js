08b516dac2478e87caececb023cea0445f94b9ce
holyhu <83695604@qq.com>
2016-01-19 16:45:04 +0800

    用户收藏下拉加载

--- server/controller/membercollect.js
+++ server/controller/membercollect.js
@@ -4,11 +4,29 @@ var _ = require("lodash");
 var util = require("../lib/util");
 var bluebird = require("bluebird");
 var CollectApp = util.getSharedComponent("membercollect");
+var config = require("../lib/config.js");
+
+function formatComment(object) {
+   return object.map((child,i)=>{
+        return {
+            createdAt: child.createdAt,
+            id: child.id,
+            imageUrl: config.imgServer+child.imageUrl,
+            originPrice: child.originPrice,
+            productCode: child.productCode,
+            salesPrice: child.salesPrice,
+            singleCode: child.singleCode,
+            sourceImage: config.imgServer+child.sourceImage,
+            sourceName: child.sourceName,
+            title: child.title
+        };
+    });
+}
 
 var collectlist = function(req, res, next) {
     var user = req.session.user;
     var pageIndex = req.query.pageIndex !== undefined ? Number(req.query.pageIndex) : 1;
-    var pageSize = req.query.pageSize !== undefined ? Number(req.query.pageSize) : 5;
+    var pageSize = 1;
     bluebird.props({
         memberCollectByUser: util.fetchAPI("memberCollectByUser", {
             memberId: user.memberId,
@@ -16,12 +34,12 @@ var collectlist = function(req, res, next) {
             pageSize: pageSize
         },false)
     }).then(function(ret) {
-        console.log(ret)
         if (ret.memberCollectByUser.returnCode === 0) {
             var collect = {},
                 object = ret.memberCollectByUser.object;
             collect.totalCount = object.totalCount;
-            collect.list = _.slice(object.result,0,pageIndex*pageSize);
+            collect.pageCount = Math.ceil(collect.totalCount/pageSize);
+            collect.list = object.result ? formatComment(object.result) : [];
             collect.pageIndex = pageIndex;
             collect.pageSize = pageSize;
             
--- shared/chunk/membercollect/partial/node.jsx
+++ shared/chunk/membercollect/partial/node.jsx
@@ -11,13 +11,13 @@ class Node extends Component{
                     list.map((child,key) => {
                         return (
                             <li id={child.goodId} key={key}>
-                                <a href={"/gooddetail/"+child.id}>
+                                <a href={"/gooddetail/"+child.singleCode}>
                                     <div className="col col-left">
                                         <img src={child.imageUrl} />
                                     </div>
                                     <div className="col col-right">
                                         <div className="title">{child.title}</div>
-                                        <div className="origin"><img src={child.originImageUrl} />{child.origin}</div>
+                                        <div className="origin"><img src={child.sourceImage} />{child.sourceName}</div>
                                         <div className="price">
                                             <span><i>￥</i>{child.salesPrice}</span>
                                             <span><i>￥</i>{child.originPrice}</span>
