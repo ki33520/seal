521bac9bdcebd411f4a6ce1f1fe79a87a82ea3eb
chenli <ali322@gmail.com>
2016-01-28 17:08:39 +0800

    update api

--- server/lib/config.js
+++ server/lib/config.js
@@ -12,11 +12,11 @@ var config = {
     "oathServer": "https://ssl.e9448.com",
     "imgServer":"http://imgtest.tepin.com/",
     "cacheServer":{
-        "hosts":["192.168.0.162:11211"]
+        "hosts":["memcached.hwg.youayun.cn:11211"]
     }
 };
 var runtime = process.env["NODE_ENV"];
-// runtime = "test"
+runtime = "test"
 config["runtime"] = runtime;
 if (runtime === "develop") {
     config.apiServer = "http://wsns.tepin.youayun.cn";
