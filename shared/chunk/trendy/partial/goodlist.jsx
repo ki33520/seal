5ac208171c7c5260ea1547ffb5ea436a9380fe50
ouyangli <278500368@qq.com>
2016-01-28 16:40:50 +0800

    修正暴款价格

--- shared/chunk/trendy/partial/goodlist.jsx
+++ shared/chunk/trendy/partial/goodlist.jsx
@@ -3,22 +3,33 @@ import React,{Component} from "react";
 import classNames from "classnames";
 
 class GoodList extends Component{
+    renderIcon(goods){
+        var icons = [];
+        if(goods.isSaleOut){
+            icons.push(<div className="sale-out" key="out"></div>);
+        }
+        if(goods.isFlashPrice){
+            icons.push(<div className="flash-price" key="flash"></div>);
+        }else if(goods.isMobilePrice){
+            icons.push(<div className="mobile-price" key="mobile"></div>);
+        }
+        return icons;
+    }
     renderGoods(){
         const {list} = this.props.category;
         if(list.length > 0){
-            return list.map((good,i)=>{
-                const statusClass = classNames({
-                    "soldOut":good.stock
-                });
+            return list.map((goods,i)=>{
                 return (
-                    <a href={"/gooddetail/"+good.singleCode} className="clearfix" key={i}>
-                        <img src={good.imageUrl}/>
-                        <div className={statusClass}></div>
+                    <a href={"/gooddetail/"+goods.id} className="clearfix" key={i}>
+                        <img src={goods.imageUrl}/>
+                        {this.renderIcon(goods)}
                         <div className="right">
-                            <span className="name">{good.title}</span>
-                            <span className="country"><i><img src={good.flag} /></i>{good.country}</span>
-                            <span className="nowPrice">&yen;{good.salePrice}</span>
-                            <span className="oldPrice">&yen;{good.originPrice}</span>
+                            <span className="name">{goods.title}</span>
+                            <span className="country">
+                                <i><img src={goods.sourceImageUrl} /></i>{goods.sourceName}
+                            </span>
+                            <span className="nowPrice">&yen;{goods.salesPrice}</span>
+                            <span className="oldPrice">&yen;{goods.originPrice}</span>
                         </div>
                     </a>
                 )
