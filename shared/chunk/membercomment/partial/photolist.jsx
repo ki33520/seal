71acc4e9ee9ccdfd1e9374a1ad61518b0b88beb3
holyhu <83695604@qq.com>
2016-03-12 11:14:32 +0800

    修改 swipelist 组件

--- shared/chunk/membercomment/partial/photolist.jsx
+++ shared/chunk/membercomment/partial/photolist.jsx
@@ -9,23 +9,13 @@ import Header from "../../common/header.jsx";
 import Swipelist from "../../common/swipelist.jsx";
 
 class PhotoList extends Component{
-    componentDidUpdate(){
-        var {photos} = this.props;
-        if(photos){
-            ReactDOM.findDOMNode(this.refs["swipelist"]).children[1].children[1].children[photos.activeIndex].click();
-        }
-    }
-    toggleFlag(index){
-        var {photos} = this.props;
-    }
     render(){
         var {photos,changeScene} = this.props;
         var imgList = photos && photos.data ? photos.data : [];
         var activeIndex = photos && photos.activeIndex ? photos.activeIndex : 0;
         return (
-            <Swipelist ref="swipelist" onGoBack={changeScene.bind(this,"index")} activeIndex={activeIndex} imgList={imgList} />
+            <Swipelist onGoBack={changeScene.bind(this,"index")} activeIndex={activeIndex} imgList={imgList} />
         )
-        
     }
 }
 
