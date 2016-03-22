8b7b399ec4e0dd3dfeef990ae0cd25c5dbefca7c
holyhu <83695604@qq.com>
2016-03-22 16:40:32 +0800

    HWGYDD-809

--- server/controller/membercenter.js
+++ server/controller/membercenter.js
@@ -38,7 +38,7 @@ var memberCenter = function(req, res, next) {
     var registerUrl = res.locals.registerUrl;
     var logoutUrl = res.locals.logoutUrl;
     var authorized = req.session.user !== undefined;
-
+    var ua = req.headers["user-agent"];
     if (authorized === true) {
         var user = req.session.user;
         bluebird.props({
@@ -60,6 +60,7 @@ var memberCenter = function(req, res, next) {
                 var initialState = {
                     isFetched: true,
                     isLogined: authorized,
+                    ua: ua,
                     member: member,
                     countOrder: countOrder,
                     api: {
@@ -88,6 +89,7 @@ var memberCenter = function(req, res, next) {
         var initialState = {
             isFetched: true,
             isLogined: authorized,
+            ua: ua,
             api: {
                 loginUrl: loginUrl,
                 registerUrl: registerUrl
