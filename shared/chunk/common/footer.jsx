1ff6413457e7623dc10c49556d63c709acededc5
ouyangli <278500368@qq.com>
2016-04-18 16:02:28 +0800

    update trend link

--- shared/chunk/common/footer.jsx
+++ shared/chunk/common/footer.jsx
@@ -12,7 +12,7 @@ class Footer extends Component{
                 <ul className="clearfix">
                     <li><a href="/" className={activeIndex==="0"?"nav-hover":null}><i></i>海外购</a></li>
                     <li><a href={jumpURL("polymer")} className={activeIndex==="1"?"nav-hover":null}><i></i>分类</a></li>
-                    <li><a href={jumpURL("trendy")} className={activeIndex==="2"?"nav-hover":null}><i></i>爆款</a></li>
+                    <li><a href="/explosion/explosion.html" className={activeIndex==="2"?"nav-hover":null}><i></i>爆款</a></li>
                     <li><a href={jumpURL("cart")} className={activeIndex==="3"?"nav-hover":null}><i></i>购物车</a></li>
                     <li><a href={jumpURL("membercenter")} className={activeIndex==="4"?"nav-hover":null}><i></i>个人中心</a></li>
                 </ul>
