b4cb04a5121e918671d214a0687f55bccc31c65f
chenli <ali322@gmail.com>
2016-05-04 15:59:18 +0800

    更改商品详情页税费显示

--- shared/chunk/gooddetail/partial/origin.jsx
+++ shared/chunk/gooddetail/partial/origin.jsx
@@ -25,30 +25,18 @@ class Origin extends Component{
                   <dt>运费：</dt>
                   <dd>{logistics}</dd>
                 </dl>   
-                <dl>
-                  <dt>税费：</dt>
-                  <dd>
-                        <a href="http://a.tepin.hk/h/rule/valueTable_h.html"
+                <div className="smallLine"></div>
+                <dl className="tax-panel">
+                   <dd>
+                    <a href="http://a.tepin.hk/h/rule/valueTable_h.html"
                          className="tariff">
-                            <span>税费=不含税商品单价*件数*(跨境电商综合税率+行邮税率)</span>
-                            <span>跨境电商综合税率=(消费税率+增值税率)/(1-消费税率)*0.7</span>
-                            <i className="iconfont icon-right"></i>
-                        </a>
-                    </dd>
-                </dl>
-                <div className="smallLine"></div> 
-                <dl>
-                  <dt>行邮税率：</dt>
-                  <dd>{good.showTaxRate.toFixed(2)}%</dd>
-                </dl>
-                <dl>
-                  <dt>消费税率：</dt>
-                  <dd>{good.consumerTax.toFixed(2)}%</dd>
-                </dl>
-                <dl>
-                  <dt>增值税率：</dt>
-                  <dd>{good.addedTax.toFixed(2)}%</dd>
-                </dl>
+                      <span><b>税费说明:</b>税费=不含税商品单价*件数*(跨境电商综合税率+行邮税率)</span>
+                      <span><b>本品适合跨境综合税:</b></span>
+                      <span>跨境电商综合税率=(消费税率+增值税率)/(1-消费税率)*0.7</span>
+                      <i className="iconfont icon-right"></i>
+                    </a>
+                   </dd>
+                 </dl> 
             </div>
         )
     }
