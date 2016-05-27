279f804cd61afec9ff8d920a4e7460ea830a26ce
holyhu <83695604@qq.com>
2016-05-27 18:02:28 +0800

    add html

--- shared/chunk/idcard/partial/index.jsx
+++ shared/chunk/idcard/partial/index.jsx
@@ -19,7 +19,38 @@ class Index extends Component{
                 <Header onGoBack={this.props.changeScene.bind(this,"index")}>
                     <span className="title">身份证管理</span>
                 </Header>
-                 
+                <div className="identityUpload">
+                    <div>
+                        <em>身份证姓名</em>
+                        <input type="text" placeholder="请输入身份证姓名" />
+                    </div>
+                    <div>
+                        <em>身份证号码</em>
+                        <input type="text" placeholder="请输入身份证号码" />
+                    </div>
+                    <div className="uploadArea">
+                        <em>身份证照片</em>
+                        <div className="pic_id">
+                            <span id="id_front">
+                                <img src="/client/asset/images/pic_id.jpg" />
+                                <a href="javascript:;">上传正面</a>
+                                <input accept="image/*" type="file" />
+                            </span>
+                            <span id="id_back">
+                                <img src="/client/asset/images/pic_id2.jpg" />
+                                <a href="javascript:;">上传反面</a>
+                                <input accept="image/*" type="file" />
+                            </span>
+                        </div>
+                    </div>
+                    
+                    <p className="info">说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容;说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明。</p>
+                    
+                    <footer>
+                        <a href="javascript:;" className="btn">保&nbsp;存</a>
+                    </footer>
+                    
+                </div>
             </div>
         )
     }
