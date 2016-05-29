9c28041cdb00fc022f98d35f372c86ed92f17679
holyhu <83695604@qq.com>
2016-05-29 14:51:10 +0800

    fix style

--- shared/chunk/idcard/partial/idcard.jsx
+++ shared/chunk/idcard/partial/idcard.jsx
@@ -32,49 +32,74 @@ class IDcard extends Component{
         const {idcardLIst,isFetched} = this.props.index;
         //console.log(idcardLIst,isFetched)
         if(!isFetched||idcardLIst.length==0) return null;
-        
-        return idcardLIst.map((item,i)=>{
-            let key = 'idcard-'+i;
-            return(
-                <div className="list" key={key}>
-                    <div className="listTitle">
-                        <span className="name">{item.name}</span>
-                        <span className="identity">{item.cardID}</span>
-                        <span className="attestation"><em></em>{item.statusName}</span>
-                    </div>
-                    <div className="pic_id">
-                        <span><img src={item.fontImgUrl} /></span>
-                        <span><img src={item.backImgUrl} /></span>
-                    </div>
-                    <div className="feedbackInfo">
-                        <p>身份信息审核内容反馈：反馈信息反馈信息反馈信息反馈信息反馈信息反馈信息......</p>
-                    </div>
-                    <div className="toolsArea">
-                        <a href="javascript:;" onClick={this.props.changeScene.bind(this,"updatecard",{id:item.id})} className="pen"><em></em>编辑</a>
-                        <a href="javascript:;" onClick={this.handleDelete.bind(this,item.id)}className="del"><em></em>删除</a>
-                    </div>
-                </div>
-            )
-        });
+        return (
+            <div className="list">
+                {
+                    idcardLIst.map((item,i)=>{
+                        let key = 'idcard-'+i;
+                        return (
+                            <div className="list-item" key={key}>
+                                <div className="listTitle">
+                                    <span className="name">{item.name}</span>
+                                    <span className="identity">{item.cardID}</span>
+                                    <span className="attestation"><em></em>{item.statusName}</span>
+                                </div>
+                                <div className="pic_id">
+                                    <span><img src={item.fontImgUrl} /></span>
+                                    <span><img src={item.backImgUrl} /></span>
+                                </div>
+                                <div className="feedbackInfo">
+                                    <p>身份信息审核内容反馈：反馈信息反馈信息反馈信息反馈信息反馈信息反馈信息......</p>
+                                </div>
+                                <div className="toolsArea">
+                                    <a href="javascript:;" onClick={this.props.changeScene.bind(this,"updatecard",{id:item.id})} className="pen"><em></em>编辑</a>
+                                    <a href="javascript:;" onClick={this.handleDelete.bind(this,item.id)}className="del"><em></em>删除</a>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
     }
     render(){
+        const {idcardLIst} = this.props.index;
+        if(idcardLIst.length>0){
+            return (
+                <div className="idcard-content">
+                    <Header>
+                        <span className="title">身份证管理</span>
+                    </Header>
+                    <div className="idcard-inner">
+                        <div className="blub">
+                            <p>根据海关政策规定，海外直邮的包裹需提供身份证照片进行入境申报，友阿海外购用户请如实提供身份证信息并确保所提供身份证与收货人完全一致。</p>
+                        </div>
+                        {this.renderIDcardList()}
+                    </div>
+                    <div className="addBtns">
+                        <a href="javascript:;" onClick={this.props.changeScene.bind(this,"addcard")} className="addBtn">新&nbsp;增</a>
+                    </div>
+                    <Dialog active={this.state.dialogActive} 
+                    onCancel={this.toggleDialog.bind(this)}
+                    onConfrim={this.state.dialogOnConfirm}>确定要删除吗?</Dialog>
+                </div>
+            )
+        }
         return (
             <div className="idcard-content">
                 <Header>
                     <span className="title">身份证管理</span>
                 </Header>
-                <div className="blub">
-                    <p>根据海关政策规定，海外直邮的包裹需提供身份证照片进行入境申报，友阿海外购用户请如实提供身份证信息并确保所提供身份证与收货人完全一致。</p>
+                <div className="idcard-inner">
+                    <div className="empty">
+                        <img src="/client/asset/images/empty_idcard.png" />
+                        <span>目前还没有任何身份证信息哟~</span>
+                        <a href="javascript:void(null)" className="empty_btn"
+                        onClick={this.props.changeScene.bind(this,"addcard")}>新增</a>
+                    </div>
                 </div>
-                {this.renderIDcardList()}
-                <div className="footer">
-                    <a href="javascript:;" onClick={this.props.changeScene.bind(this,"addcard")} className="btn">新&nbsp;增</a>
-                </div>
-                <Dialog active={this.state.dialogActive} 
-                onCancel={this.toggleDialog.bind(this)}
-                onConfrim={this.state.dialogOnConfirm}>确定要删除吗?</Dialog>
             </div>
-        )
+        );
     }
 }
 
--- shared/chunk/idcard/partial/updateidcard.jsx
+++ shared/chunk/idcard/partial/updateidcard.jsx
@@ -9,42 +9,43 @@ class UpdateIdcard extends Component{
     }
     render(){
         return (
-            <div className="idcard-content">
+            <div className="idcard-form-content">
                 <Header onGoBack={this.props.changeScene.bind(this,"index")}>
                     <span className="title">身份证修改</span>
-                    <span className="btn-right"><a>保存</a></span>
+                    <a className="screening">保存</a>
                 </Header>
-                <div className="identityUpload">
-                    <div>
-                        <em>身份证姓名</em>
-                        <input type="text" placeholder="请输入身份证姓名" />
-                    </div>
-                    <div>
-                        <em>身份证号码</em>
-                        <input type="text" placeholder="请输入身份证号码" />
-                    </div>
-                    <div className="uploadArea">
-                        <em>身份证照片</em>
-                        <div className="pic_id">
-                            <span id="id_front">
-                                <img src="/client/asset/images/pic_id.jpg" />
-                                <a href="javascript:;">上传正面</a>
-                                <input accept="image/*" type="file" />
-                            </span>
-                            <span id="id_back">
-                                <img src="/client/asset/images/pic_id2.jpg" />
-                                <a href="javascript:;">上传反面</a>
-                                <input accept="image/*" type="file" />
-                            </span>
+                <div className="idcard-form-inner">
+                    <div className="identityUpload">
+                        <div>
+                            <em>身份证姓名</em>
+                            <input type="text" placeholder="请输入身份证姓名" />
+                        </div>
+                        <div>
+                            <em>身份证号码</em>
+                            <input type="text" placeholder="请输入身份证号码" />
+                        </div>
+                        <div className="uploadArea">
+                            <em>身份证照片</em>
+                            <div className="pic_id">
+                                <span id="id_front">
+                                    <img src="/client/asset/images/pic_id.jpg" />
+                                    <a href="javascript:;">上传正面</a>
+                                    <input accept="image/*" type="file" />
+                                </span>
+                                <span id="id_back">
+                                    <img src="/client/asset/images/pic_id2.jpg" />
+                                    <a href="javascript:;">上传反面</a>
+                                    <input accept="image/*" type="file" />
+                                </span>
+                            </div>
+                        </div>
+                        
+                        <p className="info">说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容;说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明。</p>
+                        
+                        <div className="addBtns">
+                            <a href="javascript:;" className="addBtn">保&nbsp;存</a>
                         </div>
                     </div>
-                    
-                    <p className="info">说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容;说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明内容说明。</p>
-                    
-                    <div className="footer">
-                        <a href="javascript:;" className="btn">保&nbsp;存</a>
-                    </div>
-                    
                 </div>
             </div>
         )
